a723cec340575164efbb838b6038e2c722923d2f
matheusdacruz <matheusdcl83@gmail.com>
2025-07-30 11:21:14 -0300

    Mudança na mensagem

--- PequenaAutomacaoWPP/app.py
+++ PequenaAutomacaoWPP/app.py
@@ -7,27 +7,24 @@ import pyautogui
 webbrowser.open('https://web.whatsapp.com/')
 sleep(30)
 
-# Caminho do arquivo CSV
 csv_file_path = "/home/mathe/Desktop/Portfolio_MatheusdaCruz/PequenaAutomacaoWPP/teste.csv"
 
 with open(csv_file_path, 'r', encoding='utf-8') as arquivo_csv:
     leitor_csv = csv.reader(arquivo_csv)
-    # next(leitor_csv, None)  # Descomente se o CSV tiver cabeçalho
     for linha in leitor_csv:
         if len(linha) >= 2:
             nome = linha[0]
             numero = linha[1]
-            mensagem = f'Eu vou te enviar mensagens porque a senhora demorou pra me atender'
-            for i in range(10):
-                try:
-                    kit.sendwhatmsg_instantly(f"+{numero}", mensagem, 15, True, 3)
-                    sleep(5)  # Aguarda a mensagem ser preenchida
-                    pyautogui.press('enter')  # Envia a mensagem automaticamente
-                    print(f"Mensagem enviada para {nome}")
-                except Exception as e:
-                    print(f'Erro ao enviar mensagem para {nome}: {e}')
-                    with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
-                        arquivo.write(f'{nome}, {numero}\n')
-                sleep(10)
+            mensagem = f'Olá {nome}, tudo bem? Esta é uma mensagem automática enviada pelo meu script Python!'
+            try:
+                kit.sendwhatmsg_instantly(f"+{numero}", mensagem, 15, True, 3)
+                sleep(5)
+                pyautogui.press('enter')  
+                print(f"Mensagem enviada para {nome}")
+            except Exception as e:
+                print(f'Erro ao enviar mensagem para {nome}: {e}')
+                with open('erros.csv', 'a', newline='', encoding='utf-8') as arquivo:
+                    arquivo.write(f'{nome}, {numero}\n')
+            sleep(10)
         else:
             print(f"Linha inválida ignorada: {linha}")
