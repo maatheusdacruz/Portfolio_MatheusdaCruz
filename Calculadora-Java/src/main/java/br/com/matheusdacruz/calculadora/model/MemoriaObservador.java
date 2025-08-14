8061699d3c73cd7fd7037473f7a326ceda7e6beb
matheusdacruz <matheusdcl83@gmail.com>
2025-08-14 14:05:16 -0300

    Adaptação do repositório para ser exclusivo pro web portfólio

--- Calculadora-Java/src/main/java/br/com/matheusdacruz/calculadora/model/MemoriaObservador.java
+++
@@ -1,5 +0,0 @@
-package br.com.matheusdacruz.calculadora.model;
-@FunctionalInterface
-public interface MemoriaObservador {
-    void valorAlterado(String novoValor);
-}
--- CampoMinado/src/main/java/br/com/matheusdacruz/cm/model/CampoEvento.java
+++
@@ -1,5 +0,0 @@
-package br.com.matheusdacruz.cm.model;
-
-public enum CampoEvento {
-    ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
-}
