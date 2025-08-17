a69ead5ec21e8aea8338dc632c843fd11e327acf
matheusdacruz <matheusdcl83@gmail.com>
2025-08-17 01:24:35 -0300

    algumas alteracoes

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/oi', (req, res) => {
-  res.json({ message: 'Teste' });
+app.get("/", (req, res) => {
+  res.send("Backend funcionando 🚀");
 });
 
 app.listen(PORT, () => {
